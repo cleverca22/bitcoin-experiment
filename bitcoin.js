97266c98123dfaad2923c93cb68e2a99529dde57
michael bishop <cleverca22@gmail.com>
2017-05-08 14:54:55 -0300

    changes from sep 19th 2015 that i never saved to git

--- bitcoin.js
+++ bitcoin.js
@@ -1,7 +1,53 @@
 var net = require('net');
 var crypto = require('crypto');
-var opensocket = true;
-function parsePacket(p) {
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID
+
+var connid = 0;
+var peers;
+
+function Connection(ip,port,keep) {
+	this.nick = ip+'#'+connid++;
+	var socket = net.createConnection(port,ip);
+	this.socket = socket;
+	socket.on('data',function (input) {
+		try {
+			if (input.length < 20) {
+				peers.update({_id:this._id},{$set:{unreachable:true,invalid:true}},this.log);
+				this.log('invalid packet');
+				this.socket.destroy();
+				return;
+			}
+			this.parsePacket(input);
+		} catch (err) {
+			this.log(input);
+			throw err;
+		}
+	}.bind(this));
+	socket.on('error',function () {
+		this.log('connection lost due to error');
+		peers.remove({_id:this._id},function () {
+			//peers.findOne(function (err,row) {
+				//console.log(row);
+				//var out = new Connection(row._id.split(':')[0],row._id.split(':')[1],true);
+			//}.bind(this));
+		}.bind(this));
+		//peers.update({_id:this._id},{$set:{unreachable:true}},this.log);
+	}.bind(this));
+	var p = makeVersion(ip,port,'47.54.167.25',0);
+	socket.write(p);
+	if (!keep) setTimeout(function() {
+		socket.destroy();
+	},120000);
+	this.log('connecting...');
+	this._id = ip+':'+port;
+}
+Connection.prototype.log = function log() {
+	var out = Array.prototype.slice.call(arguments);
+	out.unshift(this.nick+":");
+	console.log.apply(this,out);
+}
+Connection.prototype.parsePacket = function parsePacket(p) {
 	var magic = p.readUInt32LE(0);
 	if (magic != 0xd9b4bef9) return -1;
 	var obj = {};
@@ -13,27 +59,30 @@ function parsePacket(p) {
 
 	var round1 = crypto.createHash('sha256');
 	round1.update(obj.payload);
-	var hash1 = round1.digest();
+	var hash1 = round1.digest('base64');
 	
 	var round2 = crypto.createHash('sha256');
 	round2.update(hash1);
 	var hash2 = round2.digest();
 	var hashsent = p.readUInt32LE(20);
 	var hashtrim = hash2.readUInt32LE(0);
-	if (hashsent != hashtrim) return -2;
+	if (hashsent != hashtrim) {
+		// TODOthis.log('hash mismatch',hashsent,hashtrim);
+		//return -2;
+	}
 
 	switch (obj.command) {
 	case 'verack':
-		console.log('verack with size',obj.payload.length);
+		this.log('verack with size',obj.payload.length);
 		break;
 	case 'version':
-		parseVersion(obj);
+		this.parseVersion(obj);
 		break;
 	case 'inv':
-		//parseInv(obj);
+		this.parseInv(obj);
 		break;
 	case 'addr':
-		parseAddr(obj);
+		this.parseAddr(obj);
 		break;
 	default:
 		console.log(obj);
@@ -66,11 +115,14 @@ function makepacket(command,payload) {
 	payload.copy(packet,24);
 	return packet;
 }
-function makeVersion() {
+function makeVersion(destip,destport,sourceip,sourceport) {
 	var agent = "Node-js toy";
 	if (agent.length > 0xfd) process.exit(-1);
-	var p = new Buffer(86+agent.length);
-	p.writeUInt32LE(70001,0);				// 0
+	var p = new Buffer(91+agent.length);
+	p.fill(0xaa,0,91+agent.length);
+	console.log(p);
+	p.writeUInt32LE(70002,0);				// 0
+	console.log(p);
 	
 	p.writeUInt32LE(1,4); // services			// 4
 	p.writeUInt32LE(0,8); // services			// 8
@@ -78,10 +130,10 @@ function makeVersion() {
 	var now = Date.now();
 	p.writeUInt32LE(Math.floor(now/1000),12);// 12
 	p.writeUInt32LE(0,16);					// 16
-	var dest = net_addr(0,1,'127.0.0.1',8333);
+	var dest = net_addr(0,1,destip,destport);
 	dest.copy(p,20,4);					// 20
 	
-	var src = net_addr(0,1,'10.0.0.14',0);
+	var src = net_addr(0,1,sourceip,sourceport);
 	src.copy(p,46,4);						// 46->72
 	p.writeUInt32LE(0,72);					// 72->76 noonce
 	p.writeUInt32LE(0,76);					// 76->80
@@ -90,11 +142,13 @@ function makeVersion() {
 	p.write(agent,81,agent.length,'ascii');
 	p.writeUInt32LE(0,81+agent.length);
 	p.writeInt8(1,85+agent.length);
+	p.writeUInt32LE(0,86+agent.length);
+	p.writeInt8(1,90+agent.length);
 	
 	var last = makepacket('version',p);
 	return last;
 }
-function parseVersion(packet) {
+Connection.prototype.parseVersion = function parseVersion(packet) {
 	var p = packet.payload;
 	var o = {};
 	o.version = p.readUInt32LE(0);
@@ -105,9 +159,16 @@ function parseVersion(packet) {
 	var agentsize = p.readInt8(80);
 	o.agent = p.toString('ascii',81,81+agentsize);
 	o.startheight = p.readUInt32LE(81+agentsize);
-	console.log(o);
+	this.log('parseVersion',o);
+	
+	peers.update({_id:this._id},{$set:{
+		lastSeen:new Date(),
+		unreachable:false
+	},},function (err,res) {
+		this.log('set lastseen',err,res);
+	}.bind(this));
 }
-function parseInv(packet) {
+Connection.prototype.parseInv = function parseInv(packet) {
 	var p = packet.payload;
 	var o = {};
 	var count = p.readInt8(0);
@@ -121,10 +182,10 @@ function parseInv(packet) {
 		var parsed = {};
 		parsed.type = item.readUInt32LE(0);
 		parsed.id = item.slice(4,36);
-		console.log('inv item '+(x+1)+'/'+count+' type:'+parsed.type,parsed.id);
+		if (parsed.type != 1) console.log('inv item '+(x+1)+'/'+count+' type:'+parsed.type,parsed.id);
 	}
 }
-function parseAddr(packet) {
+Connection.prototype.parseAddr = function parseAddr(packet) {
 	var p = packet.payload;
 	var o = {};
 	var count = p.readInt8(0);
@@ -136,7 +197,21 @@ function parseAddr(packet) {
 	for (var x=0; x<count; x++) {
 		var item = p.slice(offset+(30*x),offset+(30*(x+1)));
 		var out = parseNetAddr(item);
-		console.log('addr item '+(x+1)+'/'+count,item,out);
+		console.log('got peer packet',out);
+		peers.insert({_id:out.ip + ':'+out.port,advertisedTS:out.ts},function (err,result) {
+			if (err) {
+				peers.update({_id:out.ip + ':'+out.port},{$set:{advertisedTS:out.ts}},function (err,res) {
+					if (err) {
+						this.log(err,result);
+						this.log('addr item '+(x+1)+'/'+count,item,out);
+						return;
+					}
+					//new Connection(out.ip,out.port,false);
+				}.bind(this));
+				return;
+			}
+			new Connection(out.ip,out.port,false);
+		}.bind(this));
 	}
 }
 function parseNetAddr(p) {
@@ -167,18 +242,13 @@ function net_addr(time,services,ip,port) {
 	p.writeUInt16LE(port,28);
 	return p;
 }
-var p = makeVersion();
-//console.log('packet size',p.length);
-//process.stdout.write(p);
-if (opensocket) {
-	var loopback = new Connection('127.0.0.1',8333);
-	loopback.socket.write(p);
-	//setTimeout(process.exit,20000);
-}
-function Connection(ip,port) {
-	var socket = net.createConnection(8333,ip);
-	this.socket = socket;
-	socket.on('data',function (input) {
-		parsePacket(input);
+MongoClient.connect('mongodb://localhost:27017/bitcoin',function (err,db) {
+	peers = db.collection('peers');
+	peers.find().limit(100).toArray(function (err,rows) {
+		//console.log(err,rows);
+		for (var i=0; i<rows.length; i++) {
+			//var out = new Connection(rows[i]._id.split(':')[0],rows[i]._id.split(':')[1],true);
+		}
 	});
-}
+	var loopback = new Connection('127.0.0.1',8333,true);
+});
